cbf741bf111e52832e7cdb53ca7e606531e283df
Lyota <korystin.aleksei@yandex.ru>
2023-05-22 19:09:31 +0300

    update project

---
+++ 1. Знакомство с Django. Подготовка и запуск проекта/first_project/app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, reverse
+from django.http import HttpResponse
+import os
+import datetime
+
+
+def home_view(request):
+    template_name = "app/home.html"
+    pages = {
+        "Главная страница": reverse("home"),
+        "Показать текущее время": "current_time/",
+        "Показать содержимое рабочей директории": "workdir/",
+    }
+    context = {"pages": pages}
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    msg = f"Текущее время: {current_time}"
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    list_files = " | ".join(os.listdir())
+    return HttpResponse(list_files)
