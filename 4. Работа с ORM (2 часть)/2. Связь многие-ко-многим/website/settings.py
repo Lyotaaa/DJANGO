f499d8ef3d1cd107fd39a24c40f02502ba177399
Lyota <korystin.aleksei@yandex.ru>
2023-05-31 20:38:55 +0300

    Удаление строки

--- 4. Работа с ORM (2 часть)/2. Связь многие-ко-многим/website/settings.py
+++ 4. Работа с ORM (2 часть)/2. Связь многие-ко-многим/website/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
 from app.info_database import info_database
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
