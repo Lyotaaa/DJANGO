83291f46d83ef4224f3b91d596dbe5921c4327e3
Lyota <korystin.aleksei@yandex.ru>
2023-06-05 23:24:59 +0300

    Обновление вывода

--- 4. Работа с ORM (2 часть)/1. Миграции/school/views.py
+++ 4. Работа с ORM (2 часть)/1. Миграции/school/views.py
@@ -9,6 +9,4 @@ def students_list(request):
     ordering = 'group'
     object_list = Student.objects.all()
     context = {"object_list": object_list}
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     return render(request, template, context)
