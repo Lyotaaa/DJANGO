9a6a16235e81a111a54680868112201bd65be513
Lyota <korystin.aleksei@yandex.ru>
2023-06-07 23:58:04 +0300

    update m2m

--- 4. Работа с ORM (2 часть)/2. Связь многие-ко-многим/articles/models.py
+++ 4. Работа с ORM (2 часть)/2. Связь многие-ко-многим/articles/models.py
@@ -14,6 +14,22 @@ class Article(models.Model):
     class Meta:
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
-
+        ordering = ["-published_at"]
+        
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=30, verbose_name="Метка")
+    article = models.ManyToManyField(Article, through="Scope", related_name="articles", verbose_name="Статьи")
+
+    class Meta:
+        verbose_name = "Метка"
+        verbose_name_plural = "Метки"
+
+    def __str__(self) -> str:
+        return self.tag_name
+class Scope(models.Model):
+    articles = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="scopes", verbose_name="Метка")
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="scopes", verbose_name="Метка")
+    is_main = models.BooleanField(verbose_name="Основная метка")
--- 4. Работа с ORM (2 часть)/2. Связь многие-ко-многим/articles/views.py
+++ 4. Работа с ORM (2 часть)/2. Связь многие-ко-многим/articles/views.py
@@ -4,9 +4,7 @@ from articles.models import Article
 
 def articles_list(request):
     template = "articles/news.html"
-    total_list = Article.objects.all()
-    context = {"object_list": total_list}
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = "-published_at"
+    total_list = Article.objects.all().order_by(ordering)
+    context = {"object_list": total_list}
     return render(request, template, context)
