461dd2bc3264441bf4dda7bdb00c633e627234ab
Lyota <korystin.aleksei@yandex.ru>
2023-06-27 23:27:50 +0300

    Update task 6

--- 6. CRUD в DRF/stocks_products/logistic/serializers.py
+++ 6. CRUD в DRF/stocks_products/logistic/serializers.py
@@ -11,7 +11,7 @@ class ProductSerializer(serializers.ModelSerializer):
 class ProductPositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = StockProduct
-        filds = ["stock", "product", "quantity", "price"]
+        fields = ["product", "quantity", "price"]
 
 
 class StockSerializer(serializers.ModelSerializer):
@@ -19,7 +19,7 @@ class StockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Stock
-        fields = ["id", "address", "products"]
+        fields = ["id", "address", "positions"]
 
     def create(self, validated_data):
         positions = validated_data.pop("positions")
@@ -32,5 +32,5 @@ class StockSerializer(serializers.ModelSerializer):
         positions = validated_data.pop("positions")
         stock = super().update(instance, validated_data)
         for position in positions:
-            StockProduct.objects.update(stock=stock, **position)
+            StockProduct.objects.update_or_create(stock=stock, product=position["product"], defaults={**position})
         return stock
