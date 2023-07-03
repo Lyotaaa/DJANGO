e6141801a71278804c2db3bd1f5dfd8d371314e2
Lyota <korystin.aleksei@yandex.ru>
2023-07-03 22:59:27 +0300

    Data Base

--- 7. Разделение доступа в DRF/api_with_restrictions/api_with_restrictions/settings.py
+++ 7. Разделение доступа в DRF/api_with_restrictions/api_with_restrictions/settings.py
@@ -95,9 +95,9 @@ WSGI_APPLICATION = "api_with_restrictions.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": DB_info()[1],
-        "USER": DB_info()[0],
-        "PASSWORD": DB_info()[2],
+        "NAME": "NAME", #DB_info()[1],
+        "USER": "USER", #DB_info()[0],
+        "PASSWORD": "PASSWORD", #DB_info()[2],
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
