06f7fab88898dab47e21adf9a67ce5e71b6b6db5
Lyota <korystin.aleksei@yandex.ru>
2023-07-06 01:19:58 +0300

    PEP8

--- 7. Разделение доступа в DRF/api_with_restrictions/advertisements/views.py
+++ 7. Разделение доступа в DRF/api_with_restrictions/advertisements/views.py
@@ -13,40 +13,41 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 
 
-
-
-
-
-
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-    queryset =  Advertisement.objects.all()
+
+    queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     filter_backends = [filters.DjangoFilterBackend]
     filterset_class = AdvertisementFilter
     pagination_class = LimitOffsetPagination
-
 
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create"] or self.request.user.is_staff:
             return [IsAuthenticated()]
 
-        if self.action in ["update", "partial_update","destroy", 'remove_from_favorites', 'add_to_favorites', 'favorites']:
+        if self.action in [
+            "update",
+            "partial_update",
+            "destroy",
+            "remove_from_favorites",
+            "add_to_favorites",
+            "favorites",
+        ]:
             return [IsAuthenticated(), IsOwnerOrReadOnly()]
 
-        if self.action == 'retrieve':
+        if self.action == "retrieve":
             return [IsNotDraftOrIsOwner()]
         return []
 
-        
     def list(self, request):
         if request.user.is_authenticated:
             queryset = Advertisement.objects.exclude(
-                Q(status='DRAFT') & ~Q(creator=request.user)
+                Q(status="DRAFT") & ~Q(creator=request.user)
             )
         else:
-            queryset = self.get_queryset().exclude(status='DRAFT')
+            queryset = self.get_queryset().exclude(status="DRAFT")
 
         queryset = self.filter_queryset(queryset)
         page = self.paginate_queryset(queryset)
@@ -60,33 +61,33 @@ class AdvertisementViewSet(ModelViewSet):
 
     @action(detail=False, methods=["get"])
     def favorites(self, request):
-        favorite_advertisements = request.user.favorites.all().values_list("advertisement__id", flat=True)
+        favorite_advertisements = request.user.favorites.all().values_list(
+            "advertisement__id", flat=True
+        )
         queryset = Advertisement.objects.filter(id__in=favorite_advertisements)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-    
-
 
     @action(detail=True, methods=["post"])
     def add_to_favorites(self, request, pk):
         advertisement = get_object_or_404(Advertisement, pk=pk)
         if request.user != advertisement.creator:
             favorite_advertisement = FavoriteAdvertisement(
-                user=request.user,
-                advertisement=advertisement
+                user=request.user, advertisement=advertisement
             )
             favorite_advertisement.save()
             return Response(status=status.HTTP_201_CREATED)
         else:
-            return Response({"detail": "Вы не можете добавить своё объявление в избранное."}, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(
+                {"detail": "Вы не можете добавить своё объявление в избранное."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
     @action(detail=True, methods=["post"])
     def remove_from_favorites(self, request, pk):
         advertisement = get_object_or_404(Advertisement, pk=pk)
         favorite_advertisement = FavoriteAdvertisement.objects.get(
-            user=request.user,
-            advertisement=advertisement
+            user=request.user, advertisement=advertisement
         )
         favorite_advertisement.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
