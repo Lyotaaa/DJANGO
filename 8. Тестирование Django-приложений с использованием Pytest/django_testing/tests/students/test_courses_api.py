5767c8d7e99abf361121ad0a4e77f68e932c27e0
Lyota <korystin.aleksei@yandex.ru>
2023-07-07 00:14:58 +0300

    update test

--- 8. Тестирование Django-приложений с использованием Pytest/django_testing/tests/students/test_courses_api.py
+++ 8. Тестирование Django-приложений с использованием Pytest/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,38 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from students.models import Student, Course
+from model_bakery import baker
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
+
+
+# """Проверка получения первого курса"""
+@pytest.mark.django_db
+def test_first_course_check(client, course_factory):
+    courses = course_factory(_quantity=10)
+    course_id = courses[6].id
+    course_name = courses[6].name
+    response = client.get(f"/courses/{courses[6].pk}/")
+    assert response.status_code == 200
+    assert response.data["id"] == course_id
+    assert response.data["name"] == course_name
+
